49dddcc43742a0aa4b175d1092ad17f93c644c65
staticf0x <staticf0x@pm.me>
2024-04-25 15:43:29 +0200

    RPM lockfile support
    This commit add initial support for RPM lockfile
    (rpms.in.yaml and rpms.lock.yaml) via lockFileMaintenance mode
    by calling rpm-lockfile-prototype script.

---
+++ lib/modules/manager/rpm/artifacts.ts
@@ -0,0 +1,76 @@
+import type { UpdateArtifact, UpdateArtifactsResult } from "../types";
+import { logger } from "../../../logger";
+import { deleteLocalFile, readLocalFile } from "../../../util/fs";
+import { exec } from '../../../util/exec';
+import type { ExecOptions } from '../../../util/exec/types';
+import { TEMPORARY_ERROR } from '../../../constants/error-messages';
+
+export async function updateArtifacts({
+  packageFileName,
+  updatedDeps,
+  newPackageFileContent,
+  config,
+}: UpdateArtifact): Promise<UpdateArtifactsResult[] | null> {
+  logger.debug(`rpm.updateArtifacts(${packageFileName})`);
+  const isLockFileMaintenance = config.updateType === 'lockFileMaintenance';
+
+  if (!isLockFileMaintenance) {
+    logger.debug('Must be in lockFileMaintenance for rpm manager');
+    return null;
+  }
+
+  let extension = packageFileName.split('.').pop();
+  let lockFileName = `rpms.lock.${extension}`;
+
+  logger.debug(`RPM lock file: ${lockFileName}`);
+
+  const existingLockFileContent = await readLocalFile(lockFileName, 'utf8');
+
+  logger.debug(`Updating ${lockFileName}`);
+
+  const cmd: string[] = [];
+
+  try {
+    await deleteLocalFile(lockFileName);
+
+    cmd.push(`rpm-lockfile-prototype ${packageFileName}`);
+
+    const execOptions: ExecOptions = {
+      cwdFile: packageFileName,
+    }
+
+    await exec(cmd, execOptions);
+
+    const newLockFileContent = await readLocalFile(lockFileName, 'utf8');
+
+    if (existingLockFileContent === newLockFileContent) {
+      logger.debug(`${lockFileName} is unchanged`);
+      return null;
+    }
+
+    logger.debug(`Returning updated ${lockFileName}`);
+
+    return [
+      {
+        file: {
+          type: 'addition',
+          path: lockFileName,
+          contents: newLockFileContent,
+        }
+      }
+    ];
+  } catch (err) {
+    if (err.message === TEMPORARY_ERROR) {
+      throw err;
+    }
+    logger.debug({ err }, `Failed to update ${lockFileName} file`);
+    return [
+      {
+        artifactError: {
+          lockFile: lockFileName,
+          stderr: `${String(err.stdout)}\n${String(err.stderr)}`,
+        },
+      },
+    ];
+  }
+}
