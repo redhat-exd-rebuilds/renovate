a23fa7f1e4f4bf84f985f1668c50764d208f9fb1
staticf0x <staticf0x@pm.me>
2024-10-18 10:15:45 +0200

    Extract RPM dependencies

--- lib/modules/manager/rpm/extract.ts
+++ lib/modules/manager/rpm/extract.ts
@@ -1,6 +1,11 @@
 import { logger } from '../../../logger';
 import { getSiblingFileName } from '../../../util/fs';
 import type { PackageFileContent } from '../types';
+import { parseSingleYaml } from '../../../util/yaml';
+import { readLocalFile } from '../../../util/fs';
+import { RedHatRPMLockfile } from './schema';
+import type { RedHatRPMLockfileDefinition } from './schema';
+import type { PackageDependency } from '../types';
 
 export async function extractPackageFile(
   content: string,
@@ -13,8 +18,40 @@ export async function extractPackageFile(
 
   logger.debug(`RPM lock file: ${lockFile}`);
 
+  let lockFileContent = await readLocalFile(lockFile, 'utf8');
+  let deps: PackageDependency[] = [];
+
+  if (lockFileContent !== null) {
+    try {
+      let lockFile: RedHatRPMLockfileDefinition = parseSingleYaml(lockFileContent, { customSchema: RedHatRPMLockfile });
+
+      logger.debug(`Lock file version: ${lockFile.lockfileVersion}`);
+
+      for (const arch of lockFile.arches) {
+        let arch_deps: PackageDependency[] = arch.packages.map((dependency) => {
+          return {
+            depName: dependency.name,
+            packageName: dependency.name,
+            currentValue: dependency.evr,
+            currentVersion: dependency.evr,
+            versioning: "rpm",
+            datasource: "rpm-lockfile",
+          }
+        });
+
+        for (const dep of arch_deps) {
+          if (deps.findIndex((d) => d.depName == dep.depName) == -1) {
+            deps.push(dep);
+          }
+        }
+      }
+    } catch (e) {
+      logger.debug({ lockFile }, `Error parsing ${lockFile}: ${e}`);
+    }
+  }
+
   return {
     lockFiles: [lockFile],
-    deps: [],
+    deps: deps,
   };
 }
