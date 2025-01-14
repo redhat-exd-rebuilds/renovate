e06c57e024deab5bc83fee2e6f3485fa1193e9f0
Lubomir Gallovic <lgallovi@redhat.com>
2025-01-14 09:24:28 +0100

    Make renovate use downstream osv-offline
    Add @mintmaker/osv-offline to the list of dependencies and modify
    container-vulnerabilities.ts to use it instead of the upstream version.
    
    Add a new configuration option, containerVulnerabilityAlerts, which
    enables OSV alerts only for containers.

--- lib/config/options/index.ts
+++ lib/config/options/index.ts
@@ -2043,6 +2043,14 @@ const options: RenovateOptions[] = [
     experimentalIssues: [20542],
   },
   {
+    name: 'containerVulnerabilityAlerts',
+    description: 'Enable vulnerability alerts for containers.',
+    type: 'boolean',
+    default: false,
+    experimental: true,
+    experimentalIssues: [20542],
+  },
+  {
     name: 'pruneBranchAfterAutomerge',
     description: 'Set to `true` to enable branch pruning after automerging.',
     type: 'boolean',
