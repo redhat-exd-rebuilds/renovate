2fe0dad8bdc8af99ba9100952faeb0dd180b8835
staticf0x <staticf0x@pm.me>
2025-02-26 13:33:32 +0100

    Add RpmVulnerabilities class

--- lib/config/types.ts
+++ lib/config/types.ts
@@ -308,6 +308,7 @@ export interface RenovateConfig
   vulnerabilityAlerts?: RenovateSharedConfig;
   osvVulnerabilityAlerts?: boolean;
   containerVulnerabilityAlerts?: boolean;
+  rpmVulnerabilityAlerts?: boolean;
   vulnerabilitySeverity?: string;
   customManagers?: CustomManager[];
   customDatasources?: Record<string, CustomDatasourceConfig>;
--- lib/workers/repository/process/extract-update.ts
+++ lib/workers/repository/process/extract-update.ts
@@ -14,6 +14,7 @@ import { extractAllDependencies } from '../extract';
 import { generateFingerprintConfig } from '../extract/extract-fingerprint-config';
 import { branchifyUpgrades } from '../updates/branchify';
 import { ContainerVulnerabilities } from './container-vulnerabilities';
+import { RpmVulnerabilities } from './rpm-vulnerabilities';
 import { fetchUpdates } from './fetch';
 import { calculateLibYears } from './libyear';
 import { sortBranches } from './sort';
@@ -239,6 +240,29 @@ async function fetchContainerVulnerabilities(
   }
 }
 
+async function fetchRpmVulnerabilities(
+  config: RenovateConfig,
+  packageFiles: Record<string, PackageFile[]>,
+): Promise<void> {
+  if (config.rpmVulnerabilityAlerts) {
+    logger.debug(
+      'fetchRpmVulnerabilities() - rpmVulnerabilityAlerts=true',
+    );
+    try {
+      const vulnerabilities = await RpmVulnerabilities.create();
+      await vulnerabilities.appendVulnerabilityPackageRules(
+        config,
+        packageFiles,
+      );
+    } catch (err) {
+      logger.warn(
+        { err },
+        'Unable to read RPM vulnerability information',
+      );
+    }
+  }
+}
+
 export async function lookup(
   config: RenovateConfig,
   packageFiles: Record<string, PackageFile[]>,
@@ -246,6 +270,7 @@ export async function lookup(
   await fetchVulnerabilities(config, packageFiles);
   await fetchUpdates(config, packageFiles);
   await fetchContainerVulnerabilities(config, packageFiles);
+  await fetchRpmVulnerabilities(config, packageFiles);
   calculateLibYears(packageFiles);
   const { branches, branchList } = await branchifyUpgrades(
     config,
