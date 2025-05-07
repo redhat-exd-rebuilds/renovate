ab6b775f4ab2f73821c3acc234ae4afc9b828a58
querti <38067220+querti@users.noreply.github.com>
2025-05-07 12:38:58 +0200

    feat: Implement redis backend for repository cache
    Fix: redis import file extensions

--- lib/util/cache/repository/types.ts
+++ lib/util/cache/repository/types.ts
@@ -175,6 +175,7 @@ export interface RepoCache {
   save(): Promise<void>;
   getData(): RepoCacheData;
   isModified(): boolean | undefined;
+  cleanup(): Promise<void>;
 }
 
 export interface RepoCacheConfig {
