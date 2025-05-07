1944716e02f22c1435c7dafc9ee0756791244b61
querti <38067220+querti@users.noreply.github.com>
2025-05-07 12:38:58 +0200

    feat: Implement redis backend for repository cache
    Fix: redis import file extensions

--- lib/workers/repository/finalize/index.ts
+++ lib/workers/repository/finalize/index.ts
@@ -21,6 +21,7 @@ export async function finalizeRepo(
   await checkReconfigureBranch(config, repoConfig);
   await pruneStaleBranches(config, branchList);
   await repositoryCache.saveCache();
+  await repositoryCache.cleanup();
   await ensureIssuesClosing();
   await clearRenovateRefs();
   PackageFiles.clear();
