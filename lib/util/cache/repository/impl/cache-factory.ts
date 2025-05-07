823ce016d8e0f72066bae04a8ff0c38132c8eb7c
querti <38067220+querti@users.noreply.github.com>
2025-05-07 12:38:58 +0200

    feat: Implement redis backend for repository cache
    Fix: redis import file extensions

--- lib/util/cache/repository/impl/cache-factory.ts
+++ lib/util/cache/repository/impl/cache-factory.ts
@@ -2,6 +2,7 @@ import type { RepositoryCacheType } from '../../../../config/types.ts';
 import { logger } from '../../../../logger/index.ts';
 import type { RepoCache } from '../types.ts';
 import { RepoCacheLocal } from './local.ts';
+import { RepoCacheRedis } from './redis.ts';
 import { RepoCacheS3 } from './s3.ts';
 
 export class CacheFactory {
@@ -11,17 +12,18 @@ export class CacheFactory {
     cacheType: RepositoryCacheType,
   ): RepoCache {
     const type = cacheType.split('://')[0].trim().toLowerCase();
-    switch (type) {
-      case 'local':
-        return new RepoCacheLocal(repository, repoFingerprint);
-      case 's3':
-        return new RepoCacheS3(repository, repoFingerprint, cacheType);
-      default:
-        logger.warn(
-          { cacheType },
-          `Repository cache type not supported using type "local" instead`,
-        );
-        return new RepoCacheLocal(repository, repoFingerprint);
+    if (type === 'local') {
+      return new RepoCacheLocal(repository, repoFingerprint);
+    } else if (type === 's3') {
+      return new RepoCacheS3(repository, repoFingerprint, cacheType);
+    } else if (type.includes('redis')) {
+      return new RepoCacheRedis(repository, repoFingerprint, cacheType);
+    } else {
+      logger.warn(
+        { cacheType },
+        `Repository cache type not supported using type "local" instead`,
+      );
+      return new RepoCacheLocal(repository, repoFingerprint);
     }
   }
 }
