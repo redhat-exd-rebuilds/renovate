4bcfd51972d6f0feac51db11c08f48b61fc15d98
Lubomir Gallovic <lgallovi@redhat.com>
2025-07-31 15:19:43 +0200

    Restore RPM LockfileMaintenance
    Restore the old code that uses LockFileMaintenance to update RPM
    lockfiles. Ensure that there are no linting errors and that the tests
    are passing. The restored code does not replace the new RPM renovating
    approach, the two approaches are expected to coexist for some time.
    
    The only modification to the restored code is the inclusion of the old
    lockfile contents in the function's returned data. This information will
    be used during branch post-processing later.
    
    A new development branch 'rpm-lockfilemaintenance-cve' will be used for
    this feature and will be merged into develop once the implementation is
    completed.

---
+++ lib/modules/manager/rpm-lockfile/artifacts.spec.ts
@@ -0,0 +1,121 @@
+import { join } from 'upath';
+import { mockExecAll } from '../../../../test/exec-util';
+import { fs } from '../../../../test/util';
+import { GlobalConfig } from '../../../config/global';
+import type { RepoGlobalConfig } from '../../../config/types';
+import * as fsUtils from '../../../util/fs';
+import { updateArtifacts } from '.';
+
+vi.mock('../../../util/fs');
+
+const adminConfig: RepoGlobalConfig = {
+  localDir: join('/tmp/github/some/repo'),
+  cacheDir: join('/tmp/cache'),
+};
+
+describe('modules/manager/rpm-lockfile/artifacts', () => {
+  describe('updateArtifacts()', () => {
+    beforeEach(() => {
+      GlobalConfig.set(adminConfig);
+    });
+
+    it('returns null if not in lockFileMaintenance', async () => {
+      expect(
+        await updateArtifacts({
+          packageFileName: 'rpms.in.yaml',
+          updatedDeps: [],
+          newPackageFileContent: '',
+          config: {
+            updateType: 'major',
+          },
+        }),
+      ).toBeNull();
+    });
+
+    it('returns null if the lock file is the same after update', async () => {
+      const execSnapshots = mockExecAll();
+
+      fs.readLocalFile.mockResolvedValue('Current rpms.lock.yaml');
+      vi.spyOn(fsUtils, 'getSiblingFileName').mockReturnValue('rpms.lock.yaml');
+
+      expect(
+        await updateArtifacts({
+          packageFileName: 'rpms.in.yaml',
+          updatedDeps: [],
+          newPackageFileContent: '',
+          config: {
+            updateType: 'lockFileMaintenance',
+          },
+        }),
+      ).toBeNull();
+
+      expect(execSnapshots).toMatchObject([
+        { cmd: 'rpm-lockfile-prototype rpms.in.yaml --outfile rpms.lock.yaml' },
+      ]);
+    });
+
+    it('returns updated rpms.lock.yaml', async () => {
+      const execSnapshots = mockExecAll();
+
+      fs.readLocalFile.mockResolvedValueOnce('Current rpms.lock.yaml');
+      fs.readLocalFile.mockResolvedValueOnce('New rpms.lock.yaml');
+      vi.spyOn(fsUtils, 'getSiblingFileName').mockReturnValue('rpms.lock.yaml');
+
+      expect(
+        await updateArtifacts({
+          packageFileName: 'rpms.in.yaml',
+          updatedDeps: [],
+          newPackageFileContent: '',
+          config: {
+            updateType: 'lockFileMaintenance',
+          },
+        }),
+      ).toEqual([
+        {
+          file: {
+            type: 'addition',
+            path: 'rpms.lock.yaml',
+            contents: 'New rpms.lock.yaml',
+            previousContents: 'Current rpms.lock.yaml',
+          },
+        },
+      ]);
+
+      expect(execSnapshots).toMatchObject([
+        { cmd: 'rpm-lockfile-prototype rpms.in.yaml --outfile rpms.lock.yaml' },
+      ]);
+    });
+
+    it('returns updated rpms.lock.yaml for Containerfile', async () => {
+      const execSnapshots = mockExecAll();
+
+      fs.readLocalFile.mockResolvedValueOnce('Current rpms.lock.yaml');
+      fs.readLocalFile.mockResolvedValueOnce('New rpms.lock.yaml');
+      vi.spyOn(fsUtils, 'getSiblingFileName').mockReturnValue('rpms.lock.yaml');
+
+      expect(
+        await updateArtifacts({
+          packageFileName: 'rpms.in.yaml',
+          updatedDeps: [],
+          newPackageFileContent: '',
+          config: {
+            updateType: 'lockFileMaintenance',
+          },
+        }),
+      ).toEqual([
+        {
+          file: {
+            type: 'addition',
+            path: 'rpms.lock.yaml',
+            contents: 'New rpms.lock.yaml',
+            previousContents: 'Current rpms.lock.yaml',
+          },
+        },
+      ]);
+
+      expect(execSnapshots).toMatchObject([
+        { cmd: 'rpm-lockfile-prototype rpms.in.yaml --outfile rpms.lock.yaml' },
+      ]);
+    });
+  });
+});
---
+++ lib/modules/manager/rpm-lockfile/artifacts.ts
@@ -0,0 +1,88 @@
+import { TEMPORARY_ERROR } from '../../../constants/error-messages';
+import { logger } from '../../../logger';
+import { exec } from '../../../util/exec';
+import type { ExecOptions } from '../../../util/exec/types';
+import {
+  deleteLocalFile,
+  getSiblingFileName,
+  readLocalFile,
+} from '../../../util/fs';
+import type { UpdateArtifact, UpdateArtifactsResult } from '../types';
+
+export async function updateArtifacts({
+  packageFileName,
+  updatedDeps,
+  newPackageFileContent,
+  config,
+}: UpdateArtifact): Promise<UpdateArtifactsResult[] | null> {
+  logger.debug(`rpm.updateArtifacts(${packageFileName})`);
+  const isLockFileMaintenance = config.updateType === 'lockFileMaintenance';
+
+  if (!isLockFileMaintenance) {
+    logger.debug('Must be in lockFileMaintenance for rpm manager');
+    return null;
+  }
+
+  const extension = packageFileName.split('.').pop();
+  const lockFileName = getSiblingFileName(
+    packageFileName,
+    `rpms.lock.${extension}`,
+  );
+
+  logger.debug(`RPM lock file: ${lockFileName}`);
+
+  const existingLockFileContent = await readLocalFile(lockFileName, 'utf8');
+
+  logger.debug(`Updating ${lockFileName}`);
+
+  const cmd: string[] = [];
+
+  try {
+    await deleteLocalFile(lockFileName);
+
+    cmd.push(
+      `rpm-lockfile-prototype ${packageFileName} --outfile ${lockFileName}`,
+    );
+
+    // Do not set cwdFile in ExecOptions, because packageFileName
+    // and lockFileName already contain the (optional) subfolder.
+    // Setting cwdFile would descend into that subfolder and
+    // we'd have it set twice.
+    const execOptions: ExecOptions = {};
+
+    await exec(cmd, execOptions);
+
+    const newLockFileContent = await readLocalFile(lockFileName, 'utf8');
+
+    if (existingLockFileContent === newLockFileContent) {
+      logger.debug(`${lockFileName} is unchanged`);
+      return null;
+    }
+
+    logger.debug(`Returning updated ${lockFileName}`);
+
+    return [
+      {
+        file: {
+          type: 'addition',
+          path: lockFileName,
+          contents: newLockFileContent,
+          previousContents: existingLockFileContent,
+        },
+      },
+    ];
+  } catch (err) {
+    if (err.message === TEMPORARY_ERROR) {
+      throw err;
+    }
+    logger.debug({ err }, `Failed to update ${lockFileName} file`);
+    return [
+      {
+        artifactError: {
+          lockFile: lockFileName,
+          stderr: `${String(err.stdout)}\n${String(err.stderr)}`,
+        },
+      },
+    ];
+  }
+}
