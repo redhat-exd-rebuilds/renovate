b7859c2a389c4cad86484d99f59b99fa0670f67f
Lubomir Gallovic <lgallovi@redhat.com>
2025-10-06 09:23:45 +0200

    Allow the use of pruneStaleBranches with MintMaker architecture
    Since MintMaker runs for each base branch separately, pruneStaleBranches
    had to be disabled because it closes PRs created for other base
    branches. This function works by comparing detected existing Renovate
    branches and branches that were processed in the Renovate run. All
    branches that belong to Renovate and were not processed are removed.
    
    This logic can be easily modifed for multiple-parallel-runs architecture
    by including only those existing branches that push to the same base
    branch. This results in a Renovate run only cleaning up the branches
    that point to the same base branch. Base branch is included in each
    branch name, so the filtering is trivial.
    
    A new config option was added that enables this functionality. If
    MintMaker ever changes its architecture, this option should be disabled
    since the pruning would no longer work correctly.

--- lib/config/types.ts
+++ lib/config/types.ts
@@ -316,6 +316,7 @@ export interface RenovateConfig
   containerVulnerabilityAlerts?: boolean;
   rpmVulnerabilityAlerts?: boolean;
   rpmVulnerabilityAutomerge?: RPMVulnerabilityAutomerge;
+  parallelRunPruneStaleBranches?: boolean;
   vulnerabilitySeverity?: string;
   customManagers?: CustomManager[];
   customDatasources?: Record<string, CustomDatasourceConfig>;
