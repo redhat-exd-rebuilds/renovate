54d7f4040e61964dc0699698ebca76359207101a
querti <38067220+querti@users.noreply.github.com>
2025-10-06 14:00:12 +0200

    feat: parallelRunPruneStaleBranches
    Allow the use of pruneStaleBranches with MintMaker architecture

--- lib/config/types.ts
+++ lib/config/types.ts
@@ -442,6 +442,7 @@ export interface RenovateConfig
   containerVulnerabilityAlerts?: boolean;
   rpmVulnerabilityAlerts?: boolean;
   rpmVulnerabilityAutomerge?: RPMVulnerabilityAutomerge;
+  parallelRunPruneStaleBranches?: boolean;
   vulnerabilitySeverity?: string;
   customManagers?: CustomManager[];
   customDatasources?: Record<string, CustomDatasourceConfig>;
