eecc53c22df3ca68fcecc002b18162e712030baf
HozifaWasfy <hozwasfy@gmail.com>
2026-06-16 15:13:09 +0200

    chore: upgrade osv-offline db to v3

--- lib/workers/repository/process/types.ts
+++ lib/workers/repository/process/types.ts
@@ -1,4 +1,4 @@
-import type { Osv } from '@renovatebot/osv-offline';
+import type { Osv } from '@mintmaker/osv-offline';
 import type { RenovateConfig } from '../../../config/types.ts';
 import type { PackageFile } from '../../../modules/manager/types.ts';
 import type { VersioningApi } from '../../../modules/versioning/index.ts';
